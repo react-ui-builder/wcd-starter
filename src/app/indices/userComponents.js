7ead63f1624052cde63ba21313426ea2d9279dd9
reactuibuilder <reactuibuilder@gmail.com>
2020-05-28 19:10:11 +0300

    Add app directory

---
+++ src/app/indices/userComponents.js
@@ -0,0 +1,2 @@
+
+export default {'usr':{'sample':{'SampleClassComponent':require('usr/sample/SampleClassComponent.comp').default,'SampleFunctionComponent':require('usr/sample/SampleFunctionComponent.comp').default,},},};
